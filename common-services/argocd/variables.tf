0e9e2a3928e9e5d495f668c4b4d46a6cc3894b16
Hein Htet Win <heinhtetwin386@gmail.com>
2025-10-25 23:18:46 +0700

    INIT tf test

---
+++ common-services/argocd/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket for ArgoCD"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default = {
+    Project     = "DevOps-Advanced-Course"
+    Environment = "dev"
+    Service     = "argocd"
+  }
+}
