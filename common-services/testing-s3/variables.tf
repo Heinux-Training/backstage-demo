173ee5efbc4b3bfa332541b3ffe4743b186bd3ed
Hein Htet Win <heinhtetwin386@gmail.com>
2025-10-25 23:59:33 +0700

    only run apply on manual

--- common-services/testing-s3/variables.tf
+++ common-services/testing-s3/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket for ArgoCD"
   type        = string
-  default     = "hhw-backstage-demo-argocd-bucket-2"
+  default     = "hhw-backstage-demo-argocd-bucket-1"
 }
 
 variable "environment" {
